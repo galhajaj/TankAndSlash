3e80210f391177311a321bab510098fc59d65191
galhajaj <galhajaj@gmail.com>
2017-07-13 21:15:50 +0300

    add surface

---
+++ Assets/Scripts/HeadquartersManager/ExitGameOnEsc_Temp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitGameOnEsc_Temp : MonoBehaviour
+{
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Surface/Board.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Board : MonoBehaviour 
+{
+	public int BoardSizeX;
+	public int BoardSizeY;
+	public Transform TilePrefab;
+
+    public List<Transform> Tiles = new List<Transform>();
+	// ======================================================================================================================================== //
+	void Start () 
+	{
+		generateTiles();
+	}
+	// ======================================================================================================================================== //
+	private void generateTiles()
+	{
+		float boardOriginX = this.transform.position.x;
+		float boardOriginY = this.transform.position.y;
+		float tileWidth = TilePrefab.GetComponent<SpriteRenderer> ().bounds.size.x;
+		float tileHeight = TilePrefab.GetComponent<SpriteRenderer> ().bounds.size.y;
+		
+		for ( int x = 0; x < BoardSizeX; x++ ) 
+		{
+			for ( int y = 0; y < BoardSizeY; y++ ) 
+			{
+				Vector3 tilePosition = new Vector3(boardOriginX + (x * tileWidth), boardOriginY + (y * tileHeight), 0);
+				Transform tile = (Transform)Instantiate(TilePrefab, tilePosition, Quaternion.identity);
+				tile.name = "Tile" + x + y;
+				tile.parent = this.transform;
+
+				tile.GetComponent<Tile>().PosX = x;
+				tile.GetComponent<Tile>().PosY = y;
+
+                Tiles.Add(tile);
+			}
+		}
+	}
+	// ======================================================================================================================================== //
+}
