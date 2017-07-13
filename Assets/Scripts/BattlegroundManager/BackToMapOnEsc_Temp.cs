f7dc83760932d18aa0b0773acdbc58dc51108c39
galhajaj <galhajaj@gmail.com>
2017-07-13 15:10:27 +0300

    change gitignore

--- Assets/Scripts/BattlegroundManager/BackToMapOnEsc_Temp.cs
+++ Assets/Scripts/BattlegroundManager/BackToMapOnEsc_Temp.cs
@@ -8,7 +8,7 @@ public class BackToMapOnEsc_Temp : MonoBehaviour
 
 	void Start ()
     {
-        Debug.Log("NOTE: PLEASE PLEASE DO NOT FORGET TO DELETE BackToMapOnEsc_Temp SCRIPT IN MANAGER... PLEASE!");
+        Debug.LogWarning("DO NOT FORGET TO DELETE BackToMapOnEsc_Temp SCRIPT IN MANAGER!");
 	}
 	
 	void Update ()
