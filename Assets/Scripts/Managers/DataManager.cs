1be55db94c84c93c8d8cf8807ce092f5b39aab01
galhajaj <galhajaj@gmail.com>
2017-07-21 17:59:48 +0300

    add serialization to params save/load!

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class DataManager : MonoBehaviour
@@ -51,24 +52,7 @@ public class DataManager : MonoBehaviour
     {
         Debug.Log("load game - load from save file");
 
-        if (!File.Exists(SAVE_FILE_PATH))
-        {
-            Debug.LogError("save file [" + SAVE_FILE_PATH + "] doesn't exist");
-            return;
-        }
-
-        using (StreamReader sr = File.OpenText(SAVE_FILE_PATH))
-        {
-            TankParams.MaxHP = Convert.ToInt32(sr.ReadLine());
-            TankParams.HP = Convert.ToInt32(sr.ReadLine());
-            TankParams.MaxSpeed = Convert.ToDouble(sr.ReadLine());
-
-            /*string s = "";
-            while ((s = sr.ReadLine()) != null)
-            {
-                Console.WriteLine(s);
-            }*/
-        }
+        TankParams = ReadFromBinaryFile<TankParamsData>(SAVE_FILE_PATH);
     }
     // =========================================================================================== //
     // create/update the current player save file
@@ -76,11 +60,88 @@ public class DataManager : MonoBehaviour
     {
         Debug.Log("save data to file");
 
-        using (StreamWriter sw = File.CreateText(SAVE_FILE_PATH))
+        WriteToBinaryFile<TankParamsData>(SAVE_FILE_PATH, TankParams);
+    }
+    // =========================================================================================== //
+    /// <summary>
+    /// Writes the given object instance to a binary file.
+    /// <para>Object type (and all child types) must be decorated with the [Serializable] attribute.</para>
+    /// <para>To prevent a variable from being serialized, decorate it with the [NonSerialized] attribute; cannot be applied to properties.</para>
+    /// </summary>
+    /// <typeparam name="T">The type of object being written to the XML file.</typeparam>
+    /// <param name="filePath">The file path to write the object instance to.</param>
+    /// <param name="objectToWrite">The object instance to write to the XML file.</param>
+    /// <param name="append">If false the file will be overwritten if it already exists. If true the contents will be appended to the file.</param>
+    public static void WriteToBinaryFile<T>(string filePath, T objectToWrite, bool append = false)
+    {
+        using (Stream stream = File.Open(filePath, append ? FileMode.Append : FileMode.Create))
         {
-            sw.WriteLine(TankParams.MaxHP);
-            sw.WriteLine(TankParams.HP);
-            sw.WriteLine(TankParams.MaxSpeed);
+            var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            binaryFormatter.Serialize(stream, objectToWrite);
+        }
+    }
+
+    /// <summary>
+    /// Reads an object instance from a binary file.
+    /// </summary>
+    /// <typeparam name="T">The type of object to read from the XML.</typeparam>
+    /// <param name="filePath">The file path to read the object instance from.</param>
+    /// <returns>Returns a new instance of the object read from the binary file.</returns>
+    public static T ReadFromBinaryFile<T>(string filePath)
+    {
+        using (Stream stream = File.Open(filePath, FileMode.Open))
+        {
+            var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            return (T)binaryFormatter.Deserialize(stream);
+        }
+    }
+    // =========================================================================================== //
+    /// <summary>
+    /// Writes the given object instance to an XML file.
+    /// <para>Only Public properties and variables will be written to the file. These can be any type though, even other classes.</para>
+    /// <para>If there are public properties/variables that you do not want written to the file, decorate them with the [XmlIgnore] attribute.</para>
+    /// <para>Object type must have a parameterless constructor.</para>
+    /// </summary>
+    /// <typeparam name="T">The type of object being written to the file.</typeparam>
+    /// <param name="filePath">The file path to write the object instance to.</param>
+    /// <param name="objectToWrite">The object instance to write to the file.</param>
+    /// <param name="append">If false the file will be overwritten if it already exists. If true the contents will be appended to the file.</param>
+    public static void WriteToXmlFile<T>(string filePath, T objectToWrite, bool append = false) where T : new()
+    {
+        TextWriter writer = null;
+        try
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            writer = new StreamWriter(filePath, append);
+            serializer.Serialize(writer, objectToWrite);
+        }
+        finally
+        {
+            if (writer != null)
+                writer.Close();
+        }
+    }
+
+    /// <summary>
+    /// Reads an object instance from an XML file.
+    /// <para>Object type must have a parameterless constructor.</para>
+    /// </summary>
+    /// <typeparam name="T">The type of object to read from the file.</typeparam>
+    /// <param name="filePath">The file path to read the object instance from.</param>
+    /// <returns>Returns a new instance of the object read from the XML file.</returns>
+    public static T ReadFromXmlFile<T>(string filePath) where T : new()
+    {
+        TextReader reader = null;
+        try
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            reader = new StreamReader(filePath);
+            return (T)serializer.Deserialize(reader);
+        }
+        finally
+        {
+            if (reader != null)
+                reader.Close();
         }
     }
     // =========================================================================================== //
