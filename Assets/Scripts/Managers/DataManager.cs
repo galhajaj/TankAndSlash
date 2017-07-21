caacafeccbaca9d70f63b8c7c725c372f19d72dd
galhajaj <galhajaj@gmail.com>
2017-07-21 15:11:23 +0300

    add main scene - for load from file or from initial data config

---
+++ Assets/Scripts/Managers/DataManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    void Awake()
+    {
+        
+    }
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    // in new game, load from config file (scriptable object) inside editor
+    public void LoadInitialData()
+    {
+        Debug.Log("new game - load initial data");
+    }
+
+    // in continue game, load from save file of the current player
+    public void LoadDataFromFile()
+    {
+        Debug.Log("load game - load from save file");
+    }
+
+    // create/update the current player save file
+    public void SaveDataToFile()
+    {
+        Debug.Log("save data to file");
+    }
+}
---
+++ Assets/Scripts/Misc/MakePermanent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePermanent : MonoBehaviour
+{
+    private static MakePermanent _instance;
+
+    void Awake()
+    {
+        if (!_instance)
+            _instance = this;
+        else
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
